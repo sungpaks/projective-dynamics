69f041946e614282e1b3e81798e05bc9c7c970c2
sungpaks <emforhs03150@gmail.com>
2026-08-31 12:18:38 +0900

    feat: 행렬연산 클래스 추가

---
+++ src/cloth/matrix_assembler.py
@@ -0,0 +1,50 @@
+from typing import Protocol
+
+import numpy as np
+
+
+class MatrixAssembler(Protocol):
+    def add(
+        self,
+        row: int,
+        column: int,
+        value: float,
+    ) -> None: ...
+
+
+class DiagonalMatrixAssembler:
+    """대각행렬 전용 MatrixAssembler. IdentityConstraintSet 검증용"""
+
+    def __init__(self, size: int) -> None:
+        self.values = np.zeros(size, dtype=np.float32)
+
+    def add(
+        self,
+        row: int,
+        column: int,
+        value: float,
+    ) -> None:
+        if row != column:
+            raise ValueError("DiagonalMatrixAssembler only supports diagonal entries.")
+
+        self.values[row] += value
+
+
+class DenseMatrixAssembler:
+    def __init__(self, size: int) -> None:
+        self.values = np.zeros(
+            (size, size),
+            dtype=np.float32,
+        )
+
+    def add(
+        self,
+        row: int,
+        column: int,
+        value: float,
+    ) -> None:
+        self.values[row, column] += value
+
+
+# TODO: SparseMatrixAssembler
+# TODO: add를 갖는 Protocol? 인터페이스?
